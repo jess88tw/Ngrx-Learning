1f0981c7bae84c7ba785ef8f406734178a26de33
jess88tw <jess.chen@eunodata.com>
2024-07-09 17:49:22 +0800

    Refactor: imports in counter.actions.ts

--- src/app/state/counter.actions.ts
+++ src/app/state/counter.actions.ts
@@ -1,9 +1,4 @@
-import {
-  createAction,
-  createActionGroup,
-  emptyProps,
-  props,
-} from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 
 // export const setCount = createAction(
 //   '[Counter] Set Count',
