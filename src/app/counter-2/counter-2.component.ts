932b7e87e4198cbe694d69623f848282112bfcba
jess88tw <jess.chen@eunodata.com>
2024-07-09 17:49:34 +0800

    Update: counter-2 component with ngrx and core signal

--- src/app/counter-2/counter-2.component.ts
+++ src/app/counter-2/counter-2.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
 import { CounterActions } from '../state/counter.actions';
 import { CounterState } from '../state/counter.reducer';
@@ -15,8 +16,17 @@ import { selectCount } from '../state/counter.selectors';
 export class Counter2Component {
   private readonly store: Store<{ counter: CounterState }> = inject(Store);
 
+  // ngrx 的 observable 物件
   globalCount$ = this.store.select(selectCount);
+  // core signal 物件
   localCount = signal(0);
+  // 將 ngrx observable 轉換成 signal 物件
+  test = toSignal(this.globalCount$, { initialValue: 0 });
+  // 並且將根據 test 有變動時，重新計算 test2
+  test2 = computed(() => {
+    console.log('test2', this.test());
+    return this.test();
+  });
 
   increment() {
     this.store.dispatch(CounterActions.increment());
